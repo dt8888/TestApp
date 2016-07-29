69ad173210c728f9dc00ea010299d29b4d9c100c
董婷 <Luxurious_sun@dtmac.local>
2016-07-29 09:47:52 +0800

    11342

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Luxurious on 16/7/29.
+//  Copyright © 2016年 yohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
